8d380b08fb7f751185c0a3ae9304b1a8f8c4c82c
Erik Nilsen <enilsen16@live.com>
2015-01-27 11:07:57 -0800

    add child router

--- dist/child-router.js
+++ dist/child-router.js
@@ -1,7 +1,7 @@
 System.register(["aurelia-router"], function (_export) {
   "use strict";
 
-  var Router, _prototypeProperties, App;
+  var Router, _prototypeProperties, Welcome;
   return {
     setters: [function (_aureliaRouter) {
       Router = _aureliaRouter.Router;
@@ -12,16 +12,16 @@ System.register(["aurelia-router"], function (_export) {
         if (instanceProps) Object.defineProperties(child.prototype, instanceProps);
       };
 
-      App = (function () {
-        function App(router) {
+      Welcome = (function () {
+        function Welcome(router) {
+          this.heading = "Child Router";
           this.router = router;
-          this.router.configure(function (config) {
-            config.title = "Aurelia";
+          router.configure(function (config) {
             config.map([{ route: ["", "welcome"], moduleId: "welcome", nav: true, title: "Welcome" }, { route: "flickr", moduleId: "flickr", nav: true }, { route: "child-router", moduleId: "child-router", nav: true, title: "Child Router" }]);
           });
         }
 
-        _prototypeProperties(App, {
+        _prototypeProperties(Welcome, {
           inject: {
             value: function inject() {
               return [Router];
@@ -32,9 +32,9 @@ System.register(["aurelia-router"], function (_export) {
           }
         });
 
-        return App;
+        return Welcome;
       })();
-      _export("App", App);
+      _export("Welcome", Welcome);
     }
   };
 });
